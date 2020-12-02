18a76e7a861c305a22748a71c7c8c5ab13767edc
云栩兼听昭 <408422276@qq.com>
2020-12-02 23:00:51 +0800

    测试

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,4 @@
 /*
- * @Author: your name
- * @Date: 2020-07-30 13:42:30
- * @LastEditTime: 2020-11-22 09:59:08
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \admin\src\store\index.js
- */
-/* 
     创建reducer容器
     文件默认导入index
 */

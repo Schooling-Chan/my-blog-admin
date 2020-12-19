2de21079c260b8ff5acad7ece701a453d7e50c38
云栩兼听昭 <408422276@qq.com>
2020-12-19 14:54:13 +0800

    修改脚本

--- shell/build.sh
+++ shell/build.sh
@@ -1,10 +1,15 @@
 ###
  # @Author: JC
  # @Date: 2020-12-19 14:49:59
- # @LastEditTime: 2020-12-19 14:50:35
+ # @LastEditTime: 2020-12-19 14:53:43
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: \admin\shell\build.sh
 ###
+project=/data/code/my-blog-admin
+
+cd $project
+git pull
+
 npm install
 npm run build

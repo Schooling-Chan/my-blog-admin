e7e92f1e1e1e595c258cef1b22cfeaada172c716
云栩兼听昭 <408422276@qq.com>
2020-12-19 23:08:47 +0800

    懒加载

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
@@ -7,39 +7,48 @@ import 'antd/dist/antd.css';
 //css
 import './static/less/reset.css';
 
-// 自定义组件
-import Head from './component/head';
-import LeftNav from './component/left-nav';
-import Login from './component/login';
-
 // reducer
 import store from './store';
 
+// 自定义组件
+// import Head from './component/head';
+// import LeftNav from './component/left-nav';
+import Login from './component/login';
+const Head = lazy(() => import('./component/head'));
+const LeftNav = lazy(() => import('./component/left-nav'));
+
+
+
 // 路由
-import Blog from './routers/blog';
-import Home from './routers/home';
-import User from './routers/user';
-import Sets from './routers/sets';
+// import Blog from './routers/blog';
+// import Home from './routers/home';
+// import User from './routers/user';
+// import Sets from './routers/sets';
+const Sets = lazy(() => import('./routers/sets'));
+const User = lazy(() => import('./routers/user'));
+const Home = lazy(() => import('./routers/home'));
+const Blog = lazy(() => import('./routers/blog'));
 
 ReactDOM.render(
     <Provider store={store}>
         <HashRouter>
-            {/* 公共头部 */}
-            <Head></Head>
-            {/* 左边菜单 */}
-            <LeftNav></LeftNav>
+            <Suspense fallback={<div>Loading...</div>}>
+                {/* 公共头部 */}
+                <Head></Head>
+                {/* 左边菜单 */}
+                <LeftNav></LeftNav>
 
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/user' component={User} />
-                <Route path='/blog' component={Blog} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/sets' component={Sets} />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/user' component={User} />
+                    <Route path='/blog' component={Blog} />
+                    <Route path='/login' exact component={Login} />
+                    <Route path='/sets' component={Sets} />
 
-                {/* 非法页面 */}
-                <Redirect to={document.cookie !== '' ? "/?type=unsafe" : '/login'} />
-            </Switch>
-
+                    {/* 非法页面 */}
+                    <Redirect to={document.cookie !== '' ? "/?type=unsafe" : '/login'} />
+                </Switch>
+            </Suspense>
         </HashRouter>
 
     </Provider>,

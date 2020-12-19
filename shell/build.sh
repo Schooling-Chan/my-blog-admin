5ebb22f55dd5ce6851a9de89c77b91d0b7b56ae1
云栩兼听昭 <408422276@qq.com>
2020-12-19 14:50:46 +0800

    添加脚本

---
+++ shell/build.sh
@@ -0,0 +1,10 @@
+###
+ # @Author: JC
+ # @Date: 2020-12-19 14:49:59
+ # @LastEditTime: 2020-12-19 14:50:35
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: \admin\shell\build.sh
+###
+npm install
+npm run build

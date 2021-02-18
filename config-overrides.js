3c8aa7cb6e458873dcdeb499a96126bb47a370a0
云栩兼听昭 <408422276@qq.com>
2021-02-19 00:52:31 +0800

    分割代码

--- config-overrides.js
+++ config-overrides.js
@@ -33,6 +33,25 @@ const devServerConfig = () => (config) => {
 
 const addCompression = () => (config) => {
   if (process.env.NODE_ENV === "production") {
+    console.log(config);
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 300000, // 依赖包超过300000bit将被单独打包
+      automaticNameDelimiter: "-",
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `chunk.${packageName.replace("@", "")}`;
+          },
+          priority: 10,
+        },
+      },
+    };
     config.plugins.push(
       // gzip压缩
       new CompressionWebpackPlugin({
--- shell/build.sh
+++ shell/build.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: JC
  # @Date: 2020-12-19 14:49:59
- # @LastEditTime: 2020-12-26 23:11:20
+ # @LastEditTime: 2021-02-19 00:51:14
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: \admin\shell\build.sh
@@ -11,6 +11,6 @@ project=/data/code/my-blog-admin
 cd $project
 git pull
 
-# 图片压缩模块需要cnpm下载
-npm install
+# yarn下载
+yarn
 yarn build
